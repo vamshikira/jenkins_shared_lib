63d100db8ae77ea0ccfaa57a0c45f55e11cb6d96
vamshikira <89897392+vamshikira@users.noreply.github.com>
2023-09-27 01:46:12 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,16 @@
 def call(Map stageParams) {
  
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
+ //   checkout([
+//        $class: 'GitSCM',
+ //       branches: [[name:  stageParams.branch ]],
+ //       userRemoteConfigs: [[ url: stageParams.url ]]
+   // ])
+
+ checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs [[url: 'https://github.com/vamshikira/Java_app_3.0.git']])
   }
+
+
+
+
+
+
